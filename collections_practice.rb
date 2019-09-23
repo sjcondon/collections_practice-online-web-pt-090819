a94171b77cb1d394fdc9788e089a11ab6361f186
sarah <s.j.condon01@gmail.com>
2019-09-23 01:51:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(array)
+array.sort { |x,y| x <=> y }
+end
+  
+def sort_array_desc(array)
+array.sort { |x,y| y <=> x }
+end
 
+def sort_array_char_count(array)
+array.sort { |x,y| x.length <=> y.length }
+end
+
+def swap_elements(array)
+array.sort
+end
